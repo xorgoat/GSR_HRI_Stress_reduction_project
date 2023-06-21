7ec37acaec262ea5ed7e92daa9b3cb066523b248
xorgoat <a.l.rice@tilburguniversity.edu>
2023-06-21 22:56:57 +0200

    adding new analyses

---
+++ utaut_section_analysis.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(rstatix)
+library(ggplot2)
+library(ggpubr)
+library(ggstatsplot)
+
+robot<-read_csv('D:/Documents/S2Y3/Thesis/experiment/Data files/Robot_utaut_subsection_score.csv')
+laptop<-read_csv('D:/Documents/S2Y3/Thesis/experiment/Data files/Laptop_utaut_subsection_score.csv')
+
+
