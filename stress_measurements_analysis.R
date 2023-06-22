b7f37eb45f1a5234b0e2465c22d658e6f6dca8ff
xorgoat <a.l.rice@tilburguniversity.edu>
2023-06-22 23:57:09 +0200

    Graph cosmetic fixes

--- stress_measurements_analysis.R
+++ stress_measurements_analysis.R
@@ -13,27 +13,30 @@ laptop_psq<-read_csv('D:/Documents/S2Y3/Thesis/experiment/Data files/data/laptop
 robot_psq$Difference = robot_psq$Post - robot_psq$Pre
 laptop_psq$Difference = laptop_psq$Post - laptop_psq$Pre
 
-psq <- data.frame(values = c(robot_psq$Difference, laptop_psq$Difference),                      # Combine variables in data frame
-                   group = c(rep("Robot", 19), rep("Laptop", 19)))
 
-boxplot(psq$values~psq$group,
-        main = "Differences Between Pre and Post \nRelax Condition;\nSubjective Measurement",
-        ylab = "Condition",
-        xlab = "Differences",
-        col = c("seagreen1","#00F5FF"),
-) 
+### Graphs ###
 
 gsr <- data.frame(values = c(robot_gsr$Difference, laptop_gsr$Difference),                      # Combine variables in data frame
-                   group = c(rep("Robot", 19),
-                             rep("Laptop", 19)))
+                   group = c(rep("Robot", 19), rep("Laptop", 19)))
 
 boxplot(gsr$values~gsr$group,
-        main = "Differences Between Relaxation \n and Stress GSR Averages",
-        ylab = "Condition", xlab = "Differences",
-        col = c("seagreen1","#00F5FF"),
+        main = "Differences Between Relaxation\nand Stress GSR Averages",
+        xlab = "Condition", ylab = "Differences",
+        col = c("#f8766d","#00bfc4"),
 )  
 
+psq <- data.frame(values = c(robot_psq$Difference, laptop_psq$Difference),                      # Combine variables in data frame
+                  group = c(rep("Robot", 19), rep("Laptop", 19)))
 
+boxplot(psq$values~psq$group,
+        main = "Differences Between Relaxation\nand Stress PSQ Averages",
+        ylab = "Differences",
+        xlab = "Condition",
+        col = c("#f8766d","#00bfc4"),
+) 
+
+
+### Tests ###
 
 shapiro.test(robot_gsr$Difference)
 shapiro.test(laptop_gsr$Difference)
