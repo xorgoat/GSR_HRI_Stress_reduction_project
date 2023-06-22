94cdb25ddbcabb766528722fda48a34949e66475
xorgoat <a.l.rice@tilburguniversity.edu>
2023-06-22 14:28:11 +0200

    created boxplot for utaut subsections

--- utaut_section_analysis.R
+++ utaut_section_analysis.R
@@ -3,7 +3,22 @@ library(rstatix)
 library(ggplot2)
 library(ggpubr)
 library(ggstatsplot)
+library(reshape)
 
 utaut<-read_csv('D:/Documents/S2Y3/Thesis/experiment/Data files/Utaut_subsection_score.csv')
 
+subsections = c("Effort Expectancy","Performance Expectancy",
+                "Perceived Enjoyment","Satisfaction","Trust",
+                "Perceived Risk","Behavioral Intention")
 
+df <- gather(utaut, key = "UTAUT_Sections", value = "Scores", subsections) 
+head(df)
+
+# plot
+ggplot(data=df) + 
+  geom_boxplot( aes(x=factor(UTAUT_Sections), y=Scores, fill=Condition), position=position_dodge(1))+
+  xlab("UTAUT Sections") +
+  ggtitle("UTAUT Section Comparison between the Laptop and Robot Conditions")
+
+
+utaut$`Effort Expectancy`[utaut$Condition == "Robot"]
